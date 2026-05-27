45b12183506c88bd9c100bcfe3c141d7ba0be0a6
Weisley36 <mansourbengoumi36@gmail.com>
2026-05-27 18:30:39 +0100

    added blabla, fixed blabla

--- utils.py
+++ utils.py
@@ -1,1 +1,5 @@
-#utils.py
+def write_run_log(log_lines, result_line, filepath):
+    with open(filepath, 'r' , encoding='utf8') as f:
+        for i in log_lines:
+            f.write(i+'\n')
+        f.write(result_line+'\n')
